e08602d7013462dd30f50bbd9a5307c9c8cf32b5
Marcus Tedesco <marcustedesco@Marcus.local>
2014-04-19 15:18:32 -0400

    determines x,y coordinate through triangulation

---
+++ PositionViewController.h
@@ -0,0 +1,13 @@
+//
+//  PositionViewController.h
+//  3Dbeacons
+//
+//  Created by Marcus Tedesco on 4/19/14.
+//  Copyright (c) 2014 com.marcustedesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PositionViewController : UIViewController
+
+@end
---
+++ PositionViewController.m
@@ -0,0 +1,38 @@
+//
+//  PositionViewController.m
+//  3Dbeacons
+//
+//  Created by Marcus Tedesco on 4/19/14.
+//  Copyright (c) 2014 com.marcustedesco. All rights reserved.
+//
+
+#import "PositionViewController.h"
+
+@interface PositionViewController ()
+
+@end
+
+@implementation PositionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
